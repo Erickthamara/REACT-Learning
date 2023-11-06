26266f739b5536b944a2bbf034c217ed1cd36675
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-06 22:39:12 +0300

    Fisrst commit of this REACT APP which will be used to learn react.I replaced the original src file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Greetings = () => {
+  return (
+    <h1 id="button" className="bbtt">
+      Components in React are Functions.
+    </h1>
+  );
+};
+//export default Greetings;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greetings />);
