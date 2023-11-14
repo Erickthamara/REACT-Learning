867850ffb1d24c2235405a6d19e9a9463faa22bb
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-14 05:55:41 +0300

    Refactored props to use destructing objects in JS

--- src/index.js
+++ src/index.js
@@ -2,27 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const author = "Shana Abe";
-const book = "An American beauty";
-const image = "./Images/Image-1.jpg";
+const firstBook = {
+  author: "Shana Abe",
+  book: "An American beauty",
+  image: "./Images/Image-1.jpg",
+};
+
+const secondBook = {
+  author: "Evie Woods",
+  book: "The Lost Bookshop",
+  image: "https://m.media-amazon.com/images/I/91GYtFqr3GL._AC_UL320_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} book={book} image={image} />
-      <Book author={author} book={book} image={image} />
-      <Book author={author} book={book} image={image} />
-      <Book author={author} book={book} image={image} />
+      <Book
+        author={firstBook.author}
+        book={firstBook.book}
+        image={firstBook.image}
+      />
+      <Book
+        author={secondBook.author}
+        book={secondBook.book}
+        image={secondBook.image}
+      />
     </section>
   );
 };
 
 const Book = (props) => {
+  const { author, image, book } = props;
   return (
     <article className="book">
-      <img src={props.image} alt="An American beauty Novel" />
-      <h2>{props.book}</h2>
-      <h3>{props.author}</h3>
+      <img src={image} alt="An American beauty Novel" />
+      <h2>{book}</h2>
+      <h3>{author}</h3>
     </article>
   );
 };
