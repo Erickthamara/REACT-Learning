d42af82be7041045442fc063d8f06acd20167bf5
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-15 09:14:53 +0300

    Added a .map function onto the book list to add the books by looping through them and passing the properties t the book component

--- src/index.js
+++ src/index.js
@@ -2,42 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  author: "Shana Abe",
-  book: "An American beauty",
-  image: "./Images/Image-1.jpg",
-};
+const Books = [
+  {
+    author: "Shana Abe",
+    book: "An American beauty",
+    image: "./Images/Image-1.jpg",
+  },
 
-const secondBook = {
-  author: "Evie Woods",
-  book: "The Lost Bookshop",
-  image: "https://m.media-amazon.com/images/I/91GYtFqr3GL._AC_UL320_.jpg",
-};
+  {
+    author: "Evie Woods",
+    book: "The Lost Bookshop",
+    image: "https://m.media-amazon.com/images/I/91GYtFqr3GL._AC_UL320_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        book={firstBook.book}
-        image={firstBook.image}
-      />
-      <Book
-        author={secondBook.author}
-        book={secondBook.book}
-        image={secondBook.image}
-      />
+      {Books.map((book) => {
+        const { image, author, book: title } = book;
+        return <Book image={image} author={author} book={title} />;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { author, image, book } = props;
+  const { author, image, book, children } = props;
   return (
     <article className="book">
       <img src={image} alt="An American beauty Novel" />
       <h2>{book}</h2>
       <h3>{author}</h3>
+      {children}
     </article>
   );
 };
