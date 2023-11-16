dc53ef11708d7a127644190d2f3c9ed04f08385e
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-16 06:39:31 +0300

    Added a unique key(id) to the book objects to get rid of the error

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const Books = [
     author: "Shana Abe",
     book: "An American beauty",
     image: "./Images/Image-1.jpg",
+    id: 1,
   },
 
   {
     author: "Evie Woods",
     book: "The Lost Bookshop",
     image: "https://m.media-amazon.com/images/I/91GYtFqr3GL._AC_UL320_.jpg",
+    id: 2,
   },
 ];
 
@@ -20,8 +22,8 @@ const BookList = () => {
   return (
     <section className="booklist">
       {Books.map((book) => {
-        const { image, author, book: title } = book;
-        return <Book image={image} author={author} book={title} />;
+        const { image, author, book: title, id } = book;
+        return <Book image={image} author={author} book={title} key={id} />;
       })}
     </section>
   );
