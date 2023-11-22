7f75eaacdf3d5a0b2f536a00924e7838a4a286a2
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-22 07:09:03 +0300

    Added a function that takes in id of the book and logs its name.Using a wrapper in the child component.

--- src/index.js
+++ src/index.js
@@ -20,25 +20,40 @@ const Books = [
 
 const BookList = () => {
   const childFunc = () => console.log("I am a child function");
+  const findBook = (id) => {
+    const bookName = Books.find((book) => {
+      return book.id === id;
+    });
+    console.log(bookName.book);
+  };
+
   return (
     <section className="booklist">
       {Books.map((book) => {
         //const { image, author, book: title, id } = book;
-        return <Book {...book} key={book.id} childFunc={childFunc} />;
+        return (
+          <Book
+            {...book}
+            key={book.id}
+            childFunc={childFunc}
+            findBook={findBook}
+          />
+        );
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { author, image, book, children, childFunc } = props;
+  const { author, image, book, children, childFunc, findBook, id } = props;
+  const getSingleBook = () => findBook(id);
   return (
     <article className="book">
       <img src={image} alt="An American beauty Novel" />
       <h2>{book}</h2>
       <h3>{author}</h3>
       {/* {children} */}
-      <button onClick={childFunc}>Click Me</button>
+      <button onClick={getSingleBook}>Click Me</button>
     </article>
   );
 };
